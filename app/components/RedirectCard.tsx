1a5bd083ab90594df3d2785c2fee7fa584b8b10b
TahaShah141 <tahashah411@gmail.com>
2025-03-04 17:56:45 +0500

    change landing page

--- app/components/RedirectCard.tsx
+++ app/components/RedirectCard.tsx
@@ -2,13 +2,17 @@ import Link from "next/link"
 
 type RedirectCardProps = {
   thumbnailSrc: string
+  name: string
   href: string
 }
 
-export const RedirectCard = ({thumbnailSrc, href}: RedirectCardProps) => {
+export const RedirectCard = ({name, thumbnailSrc, href}: RedirectCardProps) => {
   return (
-    <Link href={href} className="w-full p-2 bg-sky-600 border-white border rounded-lg flex flex-col gap-2">
-      <img src={thumbnailSrc} className="rounded-md border-white border" />
+    <Link href={href} className="flex items-center relative">
+      <p className="bg-neutral-600 border border-neutral-800 text-xs sm:text-base p-1 px-2 sm:px-4 sm:py-2 rounded-full absolute left-1/2 -translate-x-1/2 bottom-1 translate-y-1/2" >{name}</p>
+      <div className="w-full p-2 bg-neutral-600 border border-neutral-800 rounded-lg flex flex-col gap-2">
+        <img src={thumbnailSrc} className="rounded-md border border-neutral-800" />
+      </div>
     </Link>
   )
 }
--- app/page.tsx
+++ app/page.tsx
@@ -1,19 +1,19 @@
-// import { RedirectCard } from "./components/RedirectCard";
+import { RedirectCard } from "./components/RedirectCard";
 
-// export default function Home() {
-//   return (
-//     <div className="h-screen w-full flex flex-col gap-2 p-4">
-//       <RedirectCard href="/handsfree" thumbnailSrc="handsfree.png" />
-//     </div>
-//   );
-// }
-
-import { redirect } from "next/navigation";
-
-export default async function Handsfree() {
-  const { node: {value: ipAddress} }: {node: {key: string, value: string}} = await (await fetch(`${process.env.NEXT_PUBLIC_BASE_URL}/api/ip`, {
-    cache: "no-store", // Ensure fresh data on every request
-  })).json();
-
-  redirect(`http://${ipAddress}:1013`);
+export default function Home() {
+  return (
+    <div className="h-screen w-full flex flex-col gap-2 p-4">
+      <RedirectCard href="/handsfree" name={"HandsFree"} thumbnailSrc="handsfree.png" />
+    </div>
+  );
 }
+
+// import { redirect } from "next/navigation";
+
+// export default async function Handsfree() {
+//   const { node: {value: ipAddress} }: {node: {key: string, value: string}} = await (await fetch(`${process.env.NEXT_PUBLIC_BASE_URL}/api/ip`, {
+//     cache: "no-store", // Ensure fresh data on every request
+//   })).json();
+
+//   redirect(`http://${ipAddress}:1013`);
+// }
